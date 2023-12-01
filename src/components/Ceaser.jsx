11a41f1f9ce5f4529389fd042a55f42060ad5ef5
brookelove <sblove@bu.edu>
2023-12-01 09:51:48 -0500

    finishing up some flush of code

--- src/components/Ceaser.jsx
+++ src/components/Ceaser.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../assets/CSS/Ceaser.css"
+
 
 const Ceaser = () => {
+  const [count, setCount] = useState(3)
+  const [answer, setAnswer] = useState(null)
+  const showHints = () => {
+    // hint 1 : what is a type of salad and a cipher
+    // hint 2 look at the salads when you hover me
+  }
+  const checkAnswer = (e) => {
+    e.preventDefault();
+    console.log(answer);
+    if(count >= 0) {
+      setCount(count-1);
+    } else {
+      console.log("send back to overview ");
+    }
+  }
+  const handleInputChange = (e) => {
+    setAnswer(e.target.value);
+  }
   return (
-    <div>Ceaser</div>
+    <div className='ceaser-cipher fullpage d-center'>
+      <div className='background'>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p> 
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p>
+        <p>🥗</p> 
+        <p>🥗</p>
+      </div>
+      <div className='card d-center'>
+      <header className='d-between'>
+        <p>ATTEMPTS LEFT : {count}</p>
+        <a>?</a>
+      </header>
+      <form className='d-center'>
+      <h2>Tbi zlj qb Dlbj</h2>
+      <input type='text' value={answer} onChange={handleInputChange}/>
+      <button onClick={checkAnswer}
+      >Send</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
